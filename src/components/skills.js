c05bdb77b9f08fa5a8d76b151bde9a0a6d51d3c0
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-12-12 10:41:45 -0700

    change order

--- src/components/skills.js
+++ src/components/skills.js
@@ -32,12 +32,13 @@ function Skills() {
           description='Self-taught React enthusiast; honed expertise via personal projects, transitioning to professional utilization.'
           experience= 'Professional, Personal'
         />
-        <Skill
-        name='React Native'
-        imageUrl='https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png'
-        description='Self-learned React Native, applied in various personal projects for practical experience.'
-        experience= 'Personal'
+         <Skill
+        name='C#'
+        imageUrl='https://cdn.icon-icons.com/icons2/2415/PNG/512/csharp_original_logo_icon_146578.png'
+        description='Acquired C# skills in school for game development, later utilized professionally in API development.'
+        experience= 'Professional, Educational'
         />
+        
         <Skill
         name='HTML5 + CSS3'
         imageUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/CSS3_and_HTML5_logos_and_wordmarks.svg/2560px-CSS3_and_HTML5_logos_and_wordmarks.svg.png'
@@ -45,10 +46,22 @@ function Skills() {
         experience='Professional, Educational, Personal'
         />
         <Skill
+        name='Git'
+        imageUrl='https://avatars.githubusercontent.com/u/18133?s=280&v=4'
+        description='Employed Git for version control across many projects, integrating with GitHub and Azure for collaboration and deployment.'
+        experience= 'Professional, Educational, Personal'
+        />
+        <Skill
         name='Node'
         imageUrl='https://miro.medium.com/v2/resize:fit:800/1*bc9pmTiyKR0WNPka2w3e0Q.png'
         description='Learned Node.js in backend development and web services classes, employing Express; applied for personal REST APIs.'
         experience= 'Educational, Personal'
+        />
+         <Skill
+        name='SQL'
+        imageUrl='https://ojt.com/wp-content/uploads/2021/08/sql.png'
+        description='Acquired SQL proficiency in database design courses, experienced in constructing relational databases using MySQL.'
+        experience='Educational'
         />
         <Skill
         name='MongoDB'
@@ -56,12 +69,7 @@ function Skills() {
         description='Gained MongoDB expertise in web service class, exploring non-relational database concepts.'
         experience= 'Educational, Personal'
         />
-        <Skill
-        name='SQL'
-        imageUrl='https://ojt.com/wp-content/uploads/2021/08/sql.png'
-        description='Acquired SQL proficiency in database design courses, experienced in constructing relational databases using MySQL.'
-        experience='Educational'
-        />
+       
         <Skill
         name='Python'
         imageUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png'
@@ -75,10 +83,10 @@ function Skills() {
         experience= 'Educational'
         />
          <Skill
-        name='C#'
-        imageUrl='https://cdn.icon-icons.com/icons2/2415/PNG/512/csharp_original_logo_icon_146578.png'
-        description='Acquired C# skills in school for game development, later utilized professionally in API development.'
-        experience= 'Professional, Educational'
+        name='React Native'
+        imageUrl='https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png'
+        description='Self-learned React Native, applied in various personal projects for practical experience.'
+        experience= 'Personal'
         />
         <Skill
         name='PHP'
@@ -98,12 +106,8 @@ function Skills() {
         description='Utilized Figma for hackathon project design and in a user experience design and research class.'
         experience= 'Educational, Personal'
         />
-        <Skill
-        name='Git'
-        imageUrl='https://avatars.githubusercontent.com/u/18133?s=280&v=4'
-        description='Employed Git for version control across many projects, integrating with GitHub and Azure for collaboration and deployment.'
-        experience= 'Professional, Educational, Personal'
-        />
+       
+        
           
         </section>
       </div>
