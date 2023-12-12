a77c6afa56a64074ed9b3bed3c0c71b7c6cca45a
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2023-12-12 10:10:18 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './components/home';
 import { About } from './components/about';
 import { Projects } from './components/projects';
@@ -7,14 +6,12 @@ import { Skills } from './components/skills';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
-        {/* You can also include a catch-all route for 404 errors */}
-        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
   );
